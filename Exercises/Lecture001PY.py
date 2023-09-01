864a4f8e0cfedf95906661f82b9c800bbd39da60
DrunkBunnyno1 <waan2112@gmail.com>
2023-09-01 22:25:54 +0200

    re arrange files and folders to correct names

---
+++ Exercises/Lecture001PY.py
@@ -0,0 +1,88 @@
+# Problem 1 and Solution.
+# Ask the user to input a number and check if this number is positive, negative or zero and print it out.
+
+"""
+num1 = input("Type a number: ")
+num1 = int(num1)
+if num1 < 0:
+    print(f"Ur number is a negativ number, ur number is {num1}")
+elif num1 > 0:
+    print(f"Ur number is a posetiv number, ur number is {num1}")
+else:
+    print(f"Ur number is zero, {num1}")
+"""
+
+# Problem 2 and Solution.
+# ask the user to input two numbers and check which one is the smallest and print it out.
+"""
+num1 = input("Type a number: ")
+num2 = input("Type a new number: ")
+num1 = int(num1)
+num2 = int(num2)
+if num1 < num2:
+    print(f"The first number u typte is a smaler number, the smaler number is {num1}")
+elif num2 < num1:
+    print(f"The secund numer u typed is the smaler number, the smaler number is {num2}")
+else:
+    print(f"I cant see witch is the smaler number ar the number equal? \nNumber 1 : {num1}, Number 2 : {num2}?????")
+"""
+
+
+# problem 3 and solution.
+# Ask the user to input three angles and check if the triangle has a right angle.
+# Your code should make sure that all three angles are valid and make up a triangle.
+
+"""
+angle1 = input("Lets chek if u have a right angle triangle! \nType the first angle: ")
+angle2 = input("Type the secund angle: ")
+angle3 = input("And finaly type the therd angle: ")
+angle1 = int(angle1)
+angle2 = int(angle2)
+angle3 = int(angle3)
+total_angel = angle1 + angle2 + angle3
+if total_angel == 180:
+    if angle1 == 90 or angle2 == 90 or angle3 == 90:
+        print("U have a right angle triangle.")
+    else:
+        print("U dont have a right angle triangle.")
+else:
+    print(f"Chek ur angels a triangle have the angel sum of 180* ur triangel have a sum of {total_angel}*")
+"""
+# Problem 4 and solution.
+# The information in the following table is stated in a medicine package. Also it is stated that for children weight is more important than age.
+#  ***Information To be put in the script: 
+# Adults & adolescent over 40KG > 12y 1-2 pills 
+# Children 26-40KG 7-12y 1/2-1pill 
+# Children 15-25KG 3-7y 1/2 pill
+# ******************************
+# Let the user input an age and a weight, the program should recommend the number of pills for the user.
+
+age = input("We whil try to rekomend the righet doseged, Please input age of recipient: ")
+weight = input("Please input the weight of the recipient in KG: ")
+age = int(age)
+weight = int(weight)
+
+if age >= 3 and weight >= 15:
+    if age >= 12 and weight >= 40:
+        print("U can give the recipient 2 pills")
+    elif weight >= 26 and age >= 7:
+        if age >= 10 and weight >= 40:
+            print("U can give the recipient 2 pills")
+        elif age <= 10 and weight >= 40:
+            print("U can give the recipient 1 pill, consoult a doktor")
+        elif age >= 7 and weight <= 40:
+            print("U can give the recipient 1 pill")
+    elif weight >= 15 and age >= 3:
+        if weight >= 40:
+            print("CONTACKT A DOKTOR PLEASE!!!!!!!!")
+        elif weight >= 30 and weight < 40:
+            print("U can give the recipient 1 pill, consult a docktor.")
+        elif weight >= 26 and weight < 30:
+            print (" U can give the recipient 1 pill, consult a doktor.")
+        elif weight >= 15 and weight <= 25:
+            print("U can give recipient 1/2 pill")
+else:
+    print("U shuld consult a doktor i cant rekomend u to give medicine")
+
+
+
