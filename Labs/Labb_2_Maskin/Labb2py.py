e2471ce792fa3b88547e7cc78ea91753316dad53
DrunkBunnyno1 <waan2112l@gmail.com>
2023-09-13 11:29:47 +0200

    tryingto convert txt to csv

--- Labs/Labb_2_Maskin/Labb2py.py
+++ Labs/Labb_2_Maskin/Labb2py.py
@@ -1,8 +1,21 @@
-"""with open("recorses\datapoints.txt" "r") as file:
+"""with open("recorses\datapoints.csv" "r") as file:
     content = file.readlines()
     print(content)"""
 
 
-with open("recorses\datapoints.txt") as file:
+"""with open("Labs\\Labb_2_Maskin\\recorses\datapoints.csv", "r") as file:
     content = file.read()
-    print(content)
+    print(content)"""
+    
+import csv
+
+pocnon = []
+
+with open("Labs\\Labb_2_Maskin\\recorses\datapoints.csv") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        print(row["width (cm)"])
+        """pocnon.append({"width (cm)": row["width (cm)"], "height (cm)": row["height (cm)"],
+                          "label (0-pichu, 1-pikachu)": row["label (0-pichu, 1-pikachu)"]})
+print(pocnon)"""
