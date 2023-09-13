5b5b72e7cfbe665075c5c44996971e8f682ccb90
DrunkBunnyno1 <waan2112l@gmail.com>
2023-09-13 10:52:12 +0200

    testingreding txt

---
+++ Labs/Labb_2_Maskin/Labb2py.py
@@ -0,0 +1,8 @@
+"""with open("recorses\datapoints.txt" "r") as file:
+    content = file.readlines()
+    print(content)"""
+
+
+with open("recorses\datapoints.txt") as file:
+    content = file.read()
+    print(content)
