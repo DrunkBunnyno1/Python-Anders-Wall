7fdae82117a6dab1e68eb603ebcf925e08e2eea0
DrunkBunnyno1 <waan2112l@gmail.com>
2023-09-13 11:54:24 +0200

    aaaaa

--- Labs/Labb_2_Maskin/Labb2py.py
+++ Labs/Labb_2_Maskin/Labb2py.py
@@ -3,19 +3,20 @@
     print(content)"""
 
 
-"""with open("Labs\\Labb_2_Maskin\\recorses\datapoints.csv", "r") as file:
+with open("Labs\\Labb_2_Maskin\\recorses\datapoints.csv", "r") as file:
     content = file.read()
-    print(content)"""
+    print(content)
     
-import csv
+"""import csv
 
 pocnon = []
 
 with open("Labs\\Labb_2_Maskin\\recorses\datapoints.csv") as file:
     reader = csv.DictReader(file)
+#print(reader)
 
     for row in reader:
-        print(row["width (cm)"])
-        """pocnon.append({"width (cm)": row["width (cm)"], "height (cm)": row["height (cm)"],
+        #print(row["width (cm)"])
+        pocnon.append({"width (cm)": row["width (cm)"], "height (cm)": row["height (cm)"],
                           "label (0-pichu, 1-pikachu)": row["label (0-pichu, 1-pikachu)"]})
 print(pocnon)"""
