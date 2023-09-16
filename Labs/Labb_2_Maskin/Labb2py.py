a1286588499745ee1ea0f8861183499ff1e5698a
DrunkBunnyno1 <waan2112@gmail.com>
2023-09-16 16:37:13 +0200

    Ploted pika and picha

--- Labs/Labb_2_Maskin/Labb2py.py
+++ Labs/Labb_2_Maskin/Labb2py.py
@@ -1,6 +1,6 @@
-"""with open("recorses\datapoints.csv" "r") as file:
-    content = file.readlines()
-    print(content)"""
+import numpy as np
+import matplotlib.pyplot as plt
+
 x = 0
 test1 = 0
 test2 = ""
@@ -9,8 +9,12 @@ with open("Labs\\Labb_2_Maskin\\recorses\datapoints.csv", "r") as file:
     """Dra ner i spilt[""\n] lägg i lista med split[,] apend to new list"""
 
 pocnon_pika = []
-poccnon_pich = []
+pocnon_pich = []
 pocnon_list = pocnon.split("\n")
+pich_x_axis = []
+pich_y_axis = []
+pich_axis = []
+pich = []
 
 for index, pokemon in enumerate(pocnon_list):
     print(f"{index}: {pokemon}")
@@ -20,12 +24,30 @@ for i in pocnon_list:
         test1 = i
     elif i[-1]== "0":
         test2 = i
-        poccnon_pich.append(i)
-for index, pokemon in enumerate(poccnon_pich):
+        pocnon_pich.append(i)
+for index, pokemon in enumerate(pocnon_pich):
     print(f"{index}: {pokemon}")
 print("\n")
 for index, pokemon in enumerate(pocnon_pika):
     print(f"{index}: {pokemon}")
 
+for i in pocnon_pich:
+    pich.append(i.split(','))
 
+for index, pokemon in enumerate(pich):
+    print(f"{index}: {pokemon}")
 
+for index, pokemon in enumerate(pich):
+    print(f"{index}: {pokemon}")
+for i in pich:
+    del pich[x][2]
+    x += 1
+for index, pokemon in enumerate(pich):
+    print(f"{index}: {pokemon}")
+
+x, y = pich.T
+  
+  
+# plot our list in X,Y coordinates
+plt.scatter(x, y)
+plt.show()
